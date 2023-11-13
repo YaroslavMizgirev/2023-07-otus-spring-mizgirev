57571e656b705321e0efd74540d5a1c014ebab1d
Yaroslav Jones <yaroslav@mizgirev.ru>
2023-11-13 22:42:09 +0300

    Refactor to spring-boot project

---
+++ spring-boot-interview/src/test/java/org/mym/springbootinterview/SpringBootInterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mym.springbootinterview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootInterviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
